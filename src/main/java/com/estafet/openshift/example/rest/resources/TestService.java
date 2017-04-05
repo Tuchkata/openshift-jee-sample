84c1b2f5c1086871d743623f788253749d73643a
Teodor Shaterov <teodor.shaterov@estafet.com>
2017-04-05 17:08:11 +0300

    Rest test

---
+++ src/main/java/com/estafet/openshift/example/rest/resources/TestService.java
@@ -0,0 +1,15 @@
+package com.estafet.openshift.example.rest.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class TestService {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String ping() {
+		return "Hello world";
+	}
+}
