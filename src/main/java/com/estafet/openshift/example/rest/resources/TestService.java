95a11246a5a7484323ec7ff866f5219275d44ae8
Teodor Shaterov <teodor.shaterov@estafet.com>
2017-04-06 09:15:57 +0300

    PostgreSQL test again

--- src/main/java/com/estafet/openshift/example/rest/resources/TestService.java
+++ src/main/java/com/estafet/openshift/example/rest/resources/TestService.java
@@ -17,7 +17,7 @@ public class TestService {
 		Connection connection = null;
 		try {
 			Class.forName("org.postgresql.Driver");
-			connection = DriverManager.getConnection("jdbc:postgresql://172.17.0.5:5432/", "test", "test");
+			connection = DriverManager.getConnection("jdbc:postgresql://172.17.0.5:5432/sampledb", "test", "test");
 		} catch (Exception e) {
 		}
 
